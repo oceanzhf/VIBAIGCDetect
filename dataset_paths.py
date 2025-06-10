51cb5ebd43659772bca200cd90c4af1d9e944a2a
oceanzhf <112921388+oceanzhf@users.noreply.github.com>
2025-06-10 10:37:23 +0800

    Add files via upload

---
+++ dataset_paths.py
@@ -0,0 +1,18 @@
+DATASET_PATHS = [
+
+
+    dict(
+        real_path='',
+        fake_path='',
+        data_mode='',
+        key='sd1.4'
+    ),
+
+    dict(
+        real_path=r"",
+        fake_path=r"",
+        data_mode='',
+        key='SDV1.5'
+    ),
+
+]
